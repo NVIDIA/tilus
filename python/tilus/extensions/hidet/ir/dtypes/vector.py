77b686fc94fb20f8e0e7b781eef423edb69eeaf2
Yaoyao Ding <yaoyaod@nvidia.com>
2025-07-22 08:30:05 -0700

    [License] Add license header and contribution guide
    1. add liencse header for all source code.
    2. add contribution guide, which includes Developer Certificate of Origin.

--- python/tilus/extensions/hidet/ir/dtypes/vector.py
+++ python/tilus/extensions/hidet/ir/dtypes/vector.py
@@ -1,3 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from hidet.ir.dtypes.floats import float16, float32
 from hidet.ir.dtypes.integer import int8, uint8, uint16, uint32, uint64
 from hidet.ir.dtypes.integer_subbyte import uint4b
