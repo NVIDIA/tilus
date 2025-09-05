991f1e34a4fd64a6004266667de7c054eb6ec309
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-09-05 23:12:30 +0000

    adding mbarrier instructions

---
+++ python/tilus/backends/emitters/cuda/mbarrier.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from hidet.ir.dtypes import boolean
+from hidet.ir.primitives.cuda.barrier import mbarrier_arrive, mbarrier_init, mbarrier_wait
+
+from tilus.backends.codegen import BaseInstEmitter, register_emitter
+from tilus.ir.instructions import ArriveBarrierInst, ArriveRemoteBarrierInst, InitBarrierInst, WaitBarrierInst
+from tilus.target import nvgpu_sm80
+
+
+@register_emitter(InitBarrierInst, target=nvgpu_sm80)
+class InitBarrierInstEmitter(BaseInstEmitter):
+    def emit(self, inst: InitBarrierInst) -> None:
+        with self.if_then(self.current_worker == 0):
+            mbarrier_init(~inst.barrier, inst.count)
+
+
+@register_emitter(ArriveBarrierInst, target=nvgpu_sm80)
+class ArriveBarrierInstEmitter(BaseInstEmitter):
+    def emit(self, inst: ArriveBarrierInst) -> None:
+        mbarrier_arrive(~inst.barrier)
+
+
+@register_emitter(ArriveRemoteBarrierInst, target=nvgpu_sm80)
+class ArriveRemoteBarrierInstEmitter(BaseInstEmitter):
+    def emit(self, inst: ArriveRemoteBarrierInst) -> None:
+        mbarrier_arrive(~inst.barrier, inst.remote_block, pred=boolean.true)
+
+
+@register_emitter(WaitBarrierInst, target=nvgpu_sm80)
+class WaitBarrierInstEmitter(BaseInstEmitter):
+    def emit(self, inst: WaitBarrierInst) -> None:
+        mbarrier_wait(~inst.barrier, inst.phase)
--- python/tilus/ir/instructions/cuda/__init__.py
+++ python/tilus/ir/instructions/cuda/__init__.py
@@ -20,6 +20,7 @@ from .cp_async import (
     CopyAsyncWaitGroupInst,
 )
 from .ldmatrix import LoadMatrixConfig, LoadMatrixInst
+from .mbarrier import ArriveBarrierInst, ArriveRemoteBarrierInst, InitBarrierInst, WaitBarrierInst
 from .mma_dot import DotInst
 from .semaphore import LockSemaphoreInst, ReleaseSemaphoreInst
 from .simt_dot import SimtDotInst
