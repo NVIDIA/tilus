0329c49d20afa4ec7c3e2c702495f960c028184c
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-09-07 05:04:17 +0000

    update
    Signed-off-by: Yaoyao Ding <dingyaoyao.cs@gmail.com>

--- scripts/sign-commits.sh
+++ scripts/sign-commits.sh
@@ -6,24 +6,5 @@ MAIN_BRANCH="main"
 git fetch origin
 BASE=$(git merge-base HEAD origin/$MAIN_BRANCH)
 
-# Find unsigned commits
-UNSIGNED_COMMITS=$(git rev-list $BASE..HEAD | while read commit; do
-    if ! git verify-commit $commit &>/dev/null; then
-        echo $commit
-    fi
-done)
-
-if [ -z "$UNSIGNED_COMMITS" ]; then
-    echo "No commits need GPG signing."
-    exit 0
-fi
-
 # Sign all unsigned commits in one rebase
-echo "Signing the following commits with GPG:"
-echo "$UNSIGNED_COMMITS"
-git rebase --exec "git commit --amend -S --no-edit" $BASE
-
-# Print signed commits
-for commit in $UNSIGNED_COMMITS; do
-    git log --format="* %h %s" -n 1 $commit
-done
+git rebase $BASE --signoff
