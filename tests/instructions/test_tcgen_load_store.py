4a9f409dce3db1018d056df53a8ef46ab726d505
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-09-19 04:34:58 +0000

    add marker to test to requires sm100
    Signed-off-by: Yaoyao Ding <dingyaoyao.cs@gmail.com>

--- tests/instructions/test_tcgen_load_store.py
+++ tests/instructions/test_tcgen_load_store.py
@@ -53,6 +53,7 @@ class Tcgen05LoadStoreExample(tilus.Script):
         self.tmem.dealloc(t_a)
 
 
+@tilus.testing.requires.nvgpu_sm100
 @pytest.mark.parametrize("dtype", [int32, float32, float16])
 @pytest.mark.parametrize("block_m", [64, 128])
 @pytest.mark.parametrize("shape", [(128, 64), (234, 567), (1234, 2345)])
