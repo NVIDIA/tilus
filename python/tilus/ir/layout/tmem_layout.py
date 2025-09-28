c0923181a21430846f15032b8c6046f4bc9b31a0
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-09-28 02:43:02 -0400

    wip

---
+++ python/tilus/ir/layout/tmem_layout.py
@@ -0,0 +1,10 @@
+import dataclasses
+from tilus.ir.node import IRNode
+
+
+@dataclasses.dataclass(frozen=True, eq=False)
+class TMemLayout(IRNode):
+    shape: tuple[int, int]
+    mode_shape: tuple[int, ...]
+    lane_modes: tuple[int, ...]
+    column_modes: tuple[int, ...]
