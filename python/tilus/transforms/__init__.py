fdf15ed5e33b08ca3beb9e5449f6188a54ce475f
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-10-21 01:53:23 +0000

    support printing tmem tensor
    Signed-off-by: Yaoyao Ding <dingyaoyao.cs@gmail.com>

--- python/tilus/transforms/__init__.py
+++ python/tilus/transforms/__init__.py
@@ -21,6 +21,7 @@ from .inject_print_instruction import inject_print_instruction_pass
 from .layout_inference import layout_inference_pass
 from .lower_load_store import lower_load_store_pass
 from .lower_param_only_expr import lower_param_only_expr_pass
+from .lower_print_tmem_tensor import lower_print_tmemory_tensor_pass
 from .lower_to_load_matrix import lower_to_load_matrix_pass
 from .scalar_analyze import analyze_scalar_pass
 
@@ -30,6 +31,7 @@ def get_default_passes() -> list[Pass]:
         declare_to_let_pass(),
         lower_param_only_expr_pass(),
         analyze_scalar_pass(),
+        lower_print_tmemory_tensor_pass(),
         layout_inference_pass(),
         lower_to_load_matrix_pass(),
         lower_load_store_pass(),
---
+++ tests/instructions/test_print_tensor.py
@@ -0,0 +1,18 @@
+import tilus
+from tilus import float32
+from tilus.testing import requires
+
+
+class PrintTmemTensor(tilus.Script):
+    def __call__(self):
+        self.attrs.blocks = 1
+        self.attrs.warps = 4
+
+        t_a = self.tcgen05.alloc(dtype=float32, shape=[128, 32])
+        self.print_tensor("t_a: ", t_a)
+
+
+@requires.nvgpu_sm100
+def test_print_tmem_tensor():
+    kernel = PrintTmemTensor()
+    kernel()
