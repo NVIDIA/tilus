4aa93d340c6b6217c8709baf9b3d5435856d8e54
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-10-21 01:39:30 +0000

    wip
    Signed-off-by: Yaoyao Ding <dingyaoyao.cs@gmail.com>

--- examples/blackwell_matmul/matmul_v0.py
+++ examples/blackwell_matmul/matmul_v0.py
@@ -36,10 +36,12 @@ class BlackwellMatmul(tilus.Script):
         g_b = self.global_view(b_ptr, dtype=float16, shape=[n_size, k_size])
         s_a = self.shared_tensor(dtype=float16, shape=[self.block_m, self.block_k])
         s_b = self.shared_tensor(dtype=float16, shape=[self.block_n, self.block_k])
-        acc = self.tcgen05.alloc(dtype=float32, shape=[self.block_m, self.block_n])
+        t_acc = self.tcgen05.alloc(dtype=float32, shape=[self.block_m, self.block_n])
 
         mbarriers = self.mbarrier.alloc(counts=[1])
         phase: uint32 = 0
+
+        self.print_tensor("t_acc: ", t_acc)
 
         for offset_k in range(0, k_size, self.block_k):
             self.copy_async(src=g_a, dst=s_a, offsets=[offset_m, offset_k])
@@ -47,29 +49,39 @@ class BlackwellMatmul(tilus.Script):
             self.copy_async_wait_all()
             self.sync()
 
-            self.tcgen05.mma(s_a, s_b.transpose(), acc)
+            self.tcgen05.mma(s_a, s_b.transpose(), t_acc)
             self.tcgen05.commit(mbarrier=mbarriers[0])
             self.mbarrier.wait(mbarriers[0], phase=phase)
             phase ^= 1
-        
-        self.tcgen05.dealloc(acc)
-        self.mbarrier.wait(mbarriers[0], phase=phase)
+
+        r_acc = self.tcgen05.load(t_acc, offsets=[0, 0], shape=[self.block_m, self.block_n])
+        g_c = self.global_view(c_ptr, dtype=float16, shape=[m_size, n_size])
+        self.store_global(g_c, r_acc.to(float16), offsets=[offset_m, offset_n])
+
+        self.tcgen05.dealloc(t_acc)
         self.sync()
 
 
 def main():
     matmul = BlackwellMatmul()
 
-    m_size, n_size, k_size = 1024, 1024, 1024
+    m_size, n_size, k_size = 128, 64, 32
 
-    a = torch.randn(m_size, k_size, dtype=torch.float16, device="cuda")
-    b = torch.randn(n_size, k_size, dtype=torch.float16, device="cuda")
+    a = torch.ones(m_size, k_size, dtype=torch.float16, device="cuda")
+    b = torch.ones(n_size, k_size, dtype=torch.float16, device="cuda")
     c = torch.empty(m_size, n_size, dtype=torch.float16, device="cuda")
 
     matmul(m_size, n_size, k_size, a, b, c)
     torch.cuda.synchronize()
 
     c_ref = a @ b.T
+
+    print(a)
+    print(b)
+    print(c_ref)
+    print(c)
+
+
     torch.testing.assert_close(c, c_ref, atol=1e-2, rtol=1e-2)
 
 
