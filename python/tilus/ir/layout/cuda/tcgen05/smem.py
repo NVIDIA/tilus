59dcfa1b7f04760aa777ab7837cbca2efe78e038
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2025-12-05 08:20:25 +0000

    wip

--- python/tilus/ir/layout/cuda/tcgen05/smem.py
+++ python/tilus/ir/layout/cuda/tcgen05/smem.py
@@ -18,9 +18,7 @@ from dataclasses import dataclass
 from typing import Literal, Optional, Sequence, cast
 
 import numpy as np
-from hidet.ir.expr import Expr, Var
 from hidet.ir.type import DataType
-from hidet.utils.py import prod
 from tilus.extensions.hidet.ir.primitives.cuda.tcgen05 import Tcgen05SwizzleMode
 from tilus.ir.layout.shared_layout import SharedLayout
 from tilus.ir.layout.utils.cute import CuteLayout, CuteSwizzle, IntTuple, SwizzledCuteLayout, cute_layout, tuple_product
@@ -303,17 +301,14 @@ def get_shared_layout_from_canonical(canonical_layout: CanonicalSharedLayout) ->
     else:
         raise ValueError(f"Unsupported major_kind: {canonical_layout.major_kind}")
 
-    def f_offset(axes: Sequence[Var]) -> Expr | int:
-        nbytes = 16 // canonical_layout.T
-        swizzle = CuteSwizzle(bbits=bbits, mbase=mbase - floor_log2(nbytes), sshift=sshift)
-        return swizzle(layout(*axes))
+    nbytes = 16 // canonical_layout.T
+    swizzle = CuteSwizzle(bbits=bbits, mbase=mbase - floor_log2(nbytes), sshift=sshift)
+    swizzled_cute_layout = SwizzledCuteLayout(layout, swizzle)
 
-    if not isinstance(layout.shape, Sequence):
-        smem_shape = [int(layout.shape)]
-    else:
-        smem_shape = [int(tuple_product(item)) for item in layout.shape]
+    assert isinstance(layout.shape, Sequence)
+    shape = [int(tuple_product(item)) for item in layout.shape]
 
-    return SharedLayout.create(shape=smem_shape, size=prod(smem_shape), f_offset=f_offset)
+    return swizzled_cute_layout.as_shared_layout(shape)
 
 
 def generate_canonical_layout(
