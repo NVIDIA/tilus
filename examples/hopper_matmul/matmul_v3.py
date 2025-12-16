002eec304d053c869c037af433593423b33bb89e
Qidong Su <soodoshll@gmail.com>
2025-12-15 23:50:31 -0500

    [Example] Warp specialization gemm for hopper (#86)
    Add warp specialization of gemm example for hopper GPU.
    
    ### Benchmark on H200
    
    w/ warp specialization:
    ```
           m      n      k   name  latency (ms)      tflops
    0   4096   4096   4096  torch      0.179568  765.386669
    1   4096   4096   4096  tilus      0.217712  631.287910
    2   4096   4096  14336  torch      0.611840  786.212620
    3   4096   4096  14336  tilus      0.679744  707.672791
    4   8192   8192   8192  torch      1.448576  759.029314
    5   8192   8192   8192  tilus      1.771616  620.626388
    6  10240  10240  10240  torch      2.862048  750.331101
    7  10240  10240  10240  tilus      3.750112  572.645213
    ```
    
    w/o warp specialization:
    ```
           m      n      k   name  latency (ms)      tflops
    0   4096   4096   4096  torch      0.179392  766.137605
    1   4096   4096   4096  tilus      0.221408  620.749733
    2   4096   4096  14336  torch      0.612464  785.411598
    3   4096   4096  14336  tilus      0.692160  694.978516
    4   8192   8192   8192  torch      1.447856  759.406751
    5   8192   8192   8192  tilus      1.840032  597.550281
    6  10240  10240  10240  torch      2.987184  718.899036
    7  10240  10240  10240  tilus      3.776592  568.630034
    ```
    
    ### Benchmark on H100
    
    Performance w/ warp specializaiton:
    ```
           m      n      k   name  latency (ms)      tflops
    0   4096   4096   4096  torch      0.285840  480.824791
    1   4096   4096   4096  tilus      0.342080  401.774306
    2   4096   4096  14336  torch      1.050144  458.067017
    3   4096   4096  14336  tilus      1.329120  361.920928
    4   8192   8192   8192  torch      2.372816  463.378361
    5   8192   8192   8192  tilus      3.895360  282.261878
    6  10240  10240  10240  torch      5.452080  393.883370
    7  10240  10240  10240  tilus      7.888288  272.236972
    ```
    
    Performance w/o warp specialization:
    ```
           m      n      k   name  latency (ms)      tflops
    0   4096   4096   4096  torch      0.286512  479.697019
    1   4096   4096   4096  tilus      0.323552  424.781656
    2   4096   4096  14336  torch      1.052896  456.869745
    3   4096   4096  14336  tilus      1.522928  315.862823
    4   8192   8192   8192  torch      2.381680  461.653800
    5   8192   8192   8192  tilus      4.800304  229.050419
    6  10240  10240  10240  torch      5.461200  393.225600
    7  10240  10240  10240  tilus      9.372672  229.121816
    ```
    
    ---------
    
    Signed-off-by: Qidong Su <soodoshll@gmail.com>

--- examples/hopper_matmul/matmul_v3.py
+++ examples/hopper_matmul/matmul_v3.py
@@ -10,12 +10,12 @@ from tilus import float16, float32, int32, uint32
 from tilus.utils import benchmark_func, cdiv
 
 
-@tilus.autotune("num_stages", [2, 3, 4])
+@tilus.autotune("num_stages", [2, 3, 4, 5, 6, 7])
 @tilus.autotune(
-    "block_m, block_n", [(64, 128), (128, 128), (128, 256), (256, 128), (256, 256)]
+    "block_m, block_n", [[128, 64], [128, 128], [128, 256], [256, 128], [256, 256]]
 )
 @tilus.autotune("block_k", [16, 32, 64])
-class MatmulWGMMAV2(tilus.Script):
+class MatmulWGMMAV3(tilus.Script):
     def __init__(
         self,
         num_stages,
@@ -42,7 +42,7 @@ class MatmulWGMMAV2(tilus.Script):
             cdiv(m_size, self.block_m),
             cdiv(n_size, self.block_n),
         ]
-        self.attrs.warps = 4
+        self.attrs.warps = 5
 
         block_m, block_n, block_k = self.block_m, self.block_n, self.block_k
         offset_m: int32 = block_m * self.blockIdx.x
@@ -54,64 +54,59 @@ class MatmulWGMMAV2(tilus.Script):
         sb = self.shared_tensor(dtype=float16, shape=[self.num_stages, block_n, block_k])
         acc = self.register_tensor(dtype=float32, shape=[block_m, block_n], init=0.0)
 
-        tma_barriers = self.mbarrier.alloc(count=[2 for _ in range(self.num_stages)])
-        phase = self.register_tensor(dtype=uint32, shape=[self.num_stages], init=0)
+        consumer_barriers = self.mbarrier.alloc(count=[2 for _ in range(self.num_stages)])
+        producer_barriers = self.mbarrier.alloc(
+            count=[128 for _ in range(self.num_stages)]
+        )
 
-        num_iters: int32 = cdiv(k_size, block_k)
-        max_num_stages: int32 = min(num_iters, self.num_stages)
-
-        for stage in range(max_num_stages):
-            offset_k = stage * self.block_k
-            with self.single_thread():
-                self.tma.global_to_shared(
-                    src=ga,
-                    dst=sa[stage],
-                    offsets=[offset_m, offset_k],
-                    mbarrier=tma_barriers[stage],
-                )
-                self.tma.global_to_shared(
-                    src=gb,
-                    dst=sb[stage],
-                    offsets=[offset_n, offset_k],
-                    mbarrier=tma_barriers[stage],
-                )
-
-        for iter in range(num_iters):
-            stage = iter % self.num_stages
-            self.mbarrier.wait(tma_barriers[stage], phase=phase[stage])
-            self.sync()
-
-            self.wgmma.fence()
-            self.wgmma.mma(sa[stage], sb[stage].transpose(), acc)
-            self.wgmma.commit_group()
-            self.wgmma.wait_group(0)
-            phase[stage] ^= 1
-
-            preload_iter = iter + self.num_stages
-            if preload_iter < num_iters:
-                preload_stage = preload_iter % self.num_stages
-                offset_k = preload_iter * self.block_k
+        with self.thread_group(thread_begin=128, num_threads=32):
+            stage: int32 = 0
+            producer_phases = self.register_tensor(
+                dtype=uint32, shape=[self.num_stages], init=1
+            )
+            for offset_k in self.range(0, k_size, block_k, unroll=self.num_stages):
+                self.mbarrier.wait(producer_barriers[stage], phase=producer_phases[stage])
+                producer_phases[stage] ^= 1
                 with self.single_thread():
                     self.tma.global_to_shared(
                         src=ga,
-                        dst=sa[preload_stage],
+                        dst=sa[stage],
                         offsets=[offset_m, offset_k],
-                        mbarrier=tma_barriers[preload_stage],
+                        mbarrier=consumer_barriers[stage],
                     )
                     self.tma.global_to_shared(
                         src=gb,
-                        dst=sb[preload_stage],
+                        dst=sb[stage],
                         offsets=[offset_n, offset_k],
-                        mbarrier=tma_barriers[preload_stage],
+                        mbarrier=consumer_barriers[stage],
                     )
+                stage = (stage + 1) % self.num_stages
+
+            for _ in self.range(min(self.num_stages, cdiv(k_size, self.block_k))):
+                self.mbarrier.wait(
+                    producer_barriers[stage], phase=producer_phases[stage]
+                )  # wait until the stage is ready to be filled
+                producer_phases[stage] ^= 1
+                stage = (stage + 1) % self.num_stages
+
+        with self.thread_group(thread_begin=0, num_threads=128):
+            consumer_phases = self.register_tensor(
+                dtype=uint32, shape=[self.num_stages], init=0
+            )
+            stage: int32 = 0
+            for offset_k in self.range(0, k_size, block_k, unroll=self.num_stages):
+                self.mbarrier.wait(consumer_barriers[stage], phase=consumer_phases[stage])
+                consumer_phases[stage] ^= 1
+                self.wgmma.fence()
+                self.wgmma.mma(sa[stage], sb[stage].transpose(), acc)
+                self.wgmma.commit_group()
+                self.wgmma.wait_group(0)
+                self.mbarrier.arrive(producer_barriers[stage])
+                stage = (stage + 1) % self.num_stages
             self.sync()
-
-        self.free_shared(sa)
-        self.free_shared(sb)
-
-        casted_acc = self.cast(acc, dtype=float16)
-        gc = self.global_view(c_ptr, dtype=float16, shape=[m_size, n_size])
-        self.store_global(gc, casted_acc, offsets=[offset_m, offset_n])
+            casted_acc = self.cast(acc, dtype=float16)
+            gc = self.global_view(c_ptr, dtype=float16, shape=[m_size, n_size])
+            self.store_global(gc, casted_acc, offsets=[offset_m, offset_n])
 
 
 def main():
@@ -125,7 +120,7 @@ def main():
 
     rows = []
     for m, n, k in workloads:
-        matmul = MatmulWGMMAV2()
+        matmul = MatmulWGMMAV3()
 
         a = (torch.rand(m, k, dtype=torch.float16).cuda() - 0.5) / math.sqrt(k)
         b = (torch.rand(n, k, dtype=torch.float16).cuda() - 0.5) / math.sqrt(k)
