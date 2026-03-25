41ba8affee125e980232e403a1699dc01208a10f
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2026-03-25 01:29:45 +0000

    fix lint & format
    Signed-off-by: Yaoyao Ding <dingyaoyao.cs@gmail.com>

--- tests/lang/test_blocks_per_cluster.py
+++ tests/lang/test_blocks_per_cluster.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import tilus
-from tilus.testing.requires import requires
+from tilus.testing import requires
 
 
 class DemoBlockCluster(tilus.Script):
